fa9e9e38bd4236c4e6546cdd16d9f3a4a2c813fa
Erinbutt <crowleyerin1995@gmail.com>
2016-10-26 19:38:54 -0400

    Add files via upload

---
+++ src/main/java/PlantsPlus/ItemDaisy.java
@@ -0,0 +1,14 @@
+package PlantsPlus.items;
+
+import PlantsPlus.Reference;
+
+import net.minecraft.item.Item;
+
+public class ItemDaisy extends Item {
+	
+	public ItemDaisy() {
+		setUnlocalizedName(Reference.MainItems.DAISY.getUnlocalizedName());
+		setRegistryName(Reference.MainItems.DAISY.getRegistryName());
+			
+		}
+	}
---
+++ src/main/java/PlantsPlus/ItemDaisyPink.java
@@ -0,0 +1,13 @@
+package PlantsPlus.items;
+
+import PlantsPlus.Reference;
+import net.minecraft.item.Item;
+
+public class ItemDaisyPink extends Item {
+
+	public ItemDaisyPink() {
+		setUnlocalizedName(Reference.MainItems.DAISYPINK.getUnlocalizedName());
+		setRegistryName(Reference.MainItems.DAISYPINK.getRegistryName());
+		
+	}
+}
