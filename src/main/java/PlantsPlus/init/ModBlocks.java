e41795f3ddfd35ade22ea9b3674899b94f39c288
Erinbutt <crowleyerin1995@gmail.com>
2016-10-26 19:36:03 -0400

    Create ModBlocks.java

---
+++ src/main/java/PlantsPlus/init/ModBlocks.java
@@ -0,0 +1,43 @@
+package PlantsPlus.init;
+
+import PlantsPlus.blocks.BlockDaisy;
+import net.minecraft.block.Block;
+
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+
+public class ModBlocks {
+	
+	public static Block daisy;
+	
+	
+	public static void init(){
+		daisy = new BlockDaisy();
+	}	
+	
+	public static void register(){
+		registerBlock(daisy);
+	}
+	
+	private static void registerBlock(Block block){
+		GameRegistry.register(block);
+		ItemBlock item = new ItemBlock(block);
+		item.setRegistryName(block.getRegistryName());
+		GameRegistry.register(item);
+		}
+	
+	private static void registerRender(Block block){
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(),"inventory"));
+	}
+
+	public static void registerRenders() {
+		registerRender(daisy);
+		
+	}
+}
